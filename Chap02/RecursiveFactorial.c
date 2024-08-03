7402d365e47957eafb2c0c79160ff8c78819cf49
hiyoons <arin9825@naver.com>
2024-08-03 15:55:46 +0900

    factorial&fibonacciFunc

--- Chap02/RecursiveFactorial.c
+++ Chap02/RecursiveFactorial.c
@@ -13,7 +13,8 @@ int main(void)
 
     printf("1! 의 정답: %d\n", Factorial(1));
     printf("2! 의 정답: %d\n", Factorial(2)); // 2*1 => n*Factorial(n-1) -> 2*facorial(1)= 2*1
-    printf("3!의 정답: %d\n", Factorial(3));  // 3*2*1 => 3*Factorial(2)=> Factorial(2) = 2* Facorial(1)=> 1
-    printf("4!의 정답: %d\n", Factorial(4));  // 4*3*2*1
+    printf("3! 의 정답: %d\n", Factorial(3)); // 3*2*1 => 3*Factorial(2)=> Factorial(2) = 2* Facorial(1)=> 1
+    printf("4! 의 정답: %d\n", Factorial(4)); // 4*3*2*1
+    printf("9! 의 정답: %d\n", Factorial(9));
     return 0;
 }
